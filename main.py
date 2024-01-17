cab63d49b5c6d21ac40f4a0d3aad3353bc5cc040
DAlexis1 <ganonbotw@gmail.com>
2024-01-18 00:33:24 +0100

    remove star import

--- main.py
+++ main.py
@@ -1,41 +1,49 @@
 from engine import metrics as m
-from testobj import *
+import testobj
+import pygame
+
 
 def main():
     pygame.init()
-    window = g.init_window(1280, 720)
+    window = testobj.g.init_window(1280, 720)
     default_font = pygame.font.SysFont(None, 24)
 
     print("wow")
 
-    o = TestObj(1, 1)
+    o = testobj.TestObj(1, 1)
     m.pixels_per_meter = 50
 
-    while g.handle_event():
+    while testobj.g.handle_event():
         mouse_x, mouse_y = pygame.mouse.get_pos()
-        mouse_pos_meter = m.window_position_to_meters_position(mouse_x, mouse_y)
+        mouse_pos_meter = m.window_position_to_meters_position(
+            mouse_x, mouse_y)
 
         # Update
 
         # Move the camera at 5 meters / sec
-        m.x_offset_pixel += -(g.is_key_down(pygame.K_RIGHT) - g.is_key_down(pygame.K_LEFT)) * m.meters_to_pixels(5) * g.deltatime
-        m.y_offset_pixel += -(g.is_key_down(pygame.K_DOWN) - g.is_key_down(pygame.K_UP)) * m.meters_to_pixels(5) * g.deltatime
+        m.x_offset_pixel += -(testobj.g.is_key_down(pygame.K_RIGHT) -
+                              testobj.g.is_key_down(pygame.K_LEFT)) * m.meters_to_pixels(5) * testobj.g.deltatime
+        m.y_offset_pixel += -(testobj.g.is_key_down(pygame.K_DOWN) -
+                              testobj.g.is_key_down(pygame.K_UP)) * m.meters_to_pixels(5) * testobj.g.deltatime
         o.update()
 
         # Process physic (maybe do this in the object's update ? idk)
-        o.process_physics(g.deltatime)
+        o.process_physics(testobj.g.deltatime)
 
         # Draw
 
-        g.window.fill((25, 25, 25))
-        gr.draw_grid()
+        testobj.g.window.fill((25, 25, 25))
+        testobj.gr.draw_grid()
         o.draw()
 
-        fps_text = default_font.render("FPS: " + str(g.get_fps()), True, (255, 255, 255))
+        fps_text = default_font.render(
+            "FPS: " + str(testobj.g.get_fps()), True, (255, 255, 255))
         window.blit(fps_text, (10, 10))
-        dt_text = default_font.render("DT: " + str(g.deltatime) + "s", True, (255, 0, 0))
+        dt_text = default_font.render(
+            "DT: " + str(testobj.g.deltatime) + "s", True, (255, 0, 0))
         window.blit(dt_text, (10, 34))
-        mouse_pos_text = default_font.render(f"Mouse : px : {mouse_x} {mouse_y} | m : {mouse_pos_meter.x} {mouse_pos_meter.y}", True, (255, 255, 255))
+        mouse_pos_text = default_font.render(
+            f"Mouse : px : {mouse_x} {mouse_y} | m : {mouse_pos_meter.x} {mouse_pos_meter.y}", True, (255, 255, 255))
         window.blit(mouse_pos_text, (10, 58))
 
         pygame.display.flip()       # put this in handle event ???
