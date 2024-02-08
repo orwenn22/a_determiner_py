4501719bc0cc0232fbca7cf72b5fef31b2421a0c
orwenn22 <46846090+orwenn22@users.noreply.github.com>
2024-02-08 11:54:41 +0100

    globals: add mouse input to event loop

--- engine/globals.py
+++ engine/globals.py
@@ -7,6 +7,8 @@ window: pygame.Surface = None       # This is the surface of the main window
 clock: pygame.time.Clock = None
 keyboard_state = []
 keys_pressed = {}
+mouse_state = []
+mouse_button_pressed = {}
 deltatime: float = 0.0      # This is in seconds
 FPS: int = 120
 zoom_changed: bool = False
@@ -22,19 +24,23 @@ def init_window(w, h) -> pygame.Surface:
 
 
 def update_keyboard_state():
-    global keyboard_state
+    global keyboard_state, mouse_state
     keyboard_state = pygame.key.get_pressed()
+    mouse_state = pygame.mouse.get_pressed()
 
 
 def handle_event() -> bool:
     global keys_pressed, running, zoom_changed
     keys_pressed.clear()
+    mouse_button_pressed.clear()
     zoom_changed = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
             keys_pressed[event.key] = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_button_pressed[event.button] = True
         # TODO : handle key up events ?
     update_keyboard_state()
     return running
@@ -55,6 +61,15 @@ def is_key_pressed(key) -> bool:
         return False
     return keys_pressed[key]
 
+
+def is_mouse_button_down(button) -> bool:
+    return mouse_state[button-1]
+
+
+def is_mouse_button_pressed(button) -> bool:
+    if button not in mouse_button_pressed:
+        return False
+    return mouse_button_pressed[button]
 
 def get_fps() -> float:
     return clock.get_fps()
