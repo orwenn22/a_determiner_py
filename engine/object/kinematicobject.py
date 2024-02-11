9084e568f2b35c6887f3a70e50bd93af2afc213d
orwenn22 <46846090+orwenn22@users.noreply.github.com>
2024-02-11 19:02:13 +0100

    Process physics : change formatting

--- engine/object/kinematicobject.py
+++ engine/object/kinematicobject.py
@@ -47,20 +47,15 @@ class KinematicObject(entityobject.EntityObject):
             return
         # F = m * a
         if self.enable_gravity:
-            self.apply_force(pyray.Vector2(0, 9.81*self.mass)
-                             )     # gravity : 9.81 m/s²
-        self.velocity = pyray.vector2_add(
-            self.velocity, pyray.vector2_scale(self.acceleration, dt))
-        self.position = pyray.vector2_add(self.position,
-                                          pyray.vector2_scale(self.velocity, dt))
+            self.apply_force(pyray.Vector2(0, 9.81*self.mass))     # gravity : 9.81 m/s²
+        self.velocity = pyray.vector2_add(self.velocity, pyray.vector2_scale(self.acceleration, dt))    # v += a*dt
+        self.position = pyray.vector2_add(self.position, pyray.vector2_scale(self.velocity, dt))        # p += v*dt
         self.acceleration = pyray.Vector2(0, 0)       # reset acceleration
 
     def apply_force(self, force: pyray.Vector2):
         # F = m * a <=> a = F/m
-        new_acceleration = pyray.vector2_scale(
-            pyray.Vector2(force.x, force.y), 1/self.mass)
-        self.acceleration = pyray.vector2_add(
-            self.acceleration, new_acceleration)
+        new_acceleration = pyray.Vector2(force.x/self.mass, force.y/self.mass)
+        self.acceleration = pyray.vector2_add(self.acceleration, new_acceleration)
 
     def draw_hitbox(self):
         """
