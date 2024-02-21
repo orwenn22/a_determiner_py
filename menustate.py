3da8de557b29a7b977e6b59a29f1f2ca86382859
DAlexis1 <ganonbotw@gmail.com>
2024-02-21 17:30:56 +0100

    move menus methods to local functions

--- menustate.py
+++ menustate.py
@@ -9,12 +9,27 @@ import creditstate
 class MenuState(state.State):
     def __init__(self):
         super().__init__()
+
+        def play_action():
+            self.manager.set_state(gameplaystate.GameplayState())
+
+        def options_action():
+            self.manager.set_state(optionstate.OptionState())
+
+        def credits_action():
+            self.manager.set_state(creditstate.CreditState())
+
+        def quit_action():
+            # TODO : unload state_manager from here or juste add a condition in the main loop
+            self.manager.unload()
+            pyray.close_window()
+
         self.widget_manager = widgetmanager.WidgetManager()
 
-        self.play = button.Button(0, -100, 150, 40, "MC", self.play_action, "Play Game")
-        self.options = button.Button(0, -50, 150, 40, "MC", self.options_action, "Options")
-        self.credits = button.Button(0, 0, 150, 40, "MC", self.credits_action, "Credits")
-        self.quit = button.Button(0, 50, 150, 40, "MC", self.quit_action, "Quit")
+        self.play = button.Button(0, -100, 150, 40, "MC", play_action, "Play Game")
+        self.options = button.Button(0, -50, 150, 40, "MC", options_action, "Options")
+        self.credits = button.Button(0, 0, 150, 40, "MC", credits_action, "Credits")
+        self.quit = button.Button(0, 50, 150, 40, "MC", quit_action, "Quit")
 
         self.title = button.Button(-50, -200, 250, 80, "MC", label="À déterminer tm")
         self.title.set_font_size(40).set_color(pyray.Color(0, 0, 0, 0)).set_font_color(pyray.Color(127, 127, 127, 255))
@@ -31,17 +46,3 @@ class MenuState(state.State):
     def draw(self):
         pyray.clear_background(pyray.BLACK)
         self.widget_manager.draw()
-
-    def play_action(self):
-        self.manager.set_state(gameplaystate.GameplayState())
-
-    def options_action(self):
-        self.manager.set_state(optionstate.OptionState())
-
-    def credits_action(self):
-        self.manager.set_state(creditstate.CreditState())
-
-    def quit_action(self):
-        # TODO : unload state_manager from here or juste add a condition in the main loop
-        self.manager.unload()
-        pyray.close_window()
--- optionstate.py
+++ optionstate.py
@@ -8,18 +8,54 @@ import key
 class OptionState(state.State):
     def __init__(self):
         super().__init__()
+
+        def return_action():
+            self.manager.set_state(menustate.MenuState())
+
+            # TODO : instead of chr(temp put something to traduce it into the name of the key)
+            # if you even let me do that, we should add another stop condition to the while loop (in the case we do it properly we should also create a class inputbutton)
+        def newleftkey():
+            pyray.poll_input_events()
+            temp = 0
+            while (not temp):
+                temp = pyray.get_key_pressed()
+                pyray.poll_input_events()
+            if temp != 0 and temp != key.jkey_right:
+                key.jkey_left = temp
+                self.choice_key_left.label = chr(temp)
+
+        def newrightkey():
+            pyray.poll_input_events()
+            temp = 0
+            while (not temp):
+                temp = pyray.get_key_pressed()
+                pyray.poll_input_events()
+            if temp != 0 and temp != key.jkey_left:
+                key.jkey_right = temp
+                self.choice_key_right.label = chr(temp)
+
+        def newactionkey():
+            pyray.poll_input_events()
+            temp = 0
+            while (not temp):
+                temp = pyray.get_key_pressed()
+                pyray.poll_input_events()
+            if temp != 0 and temp != key.action_key:
+                key.action_key = temp
+                self.choice_key_action.label = chr(temp) if temp != 32 else "Space"
+
         self.widget_manager = widgetmanager.WidgetManager()
         self.title = button.Button(50, -200, 250, 80, "MC", label="Options :")
         self.title.set_font_size(40).set_color(pyray.Color(0, 0, 0, 0)).set_font_color(pyray.Color(127, 127, 127, 255))
 
-        self.returntomenu = button.Button(0, 200, 250, 40, "MC", self.return_action, "Return to main menu")
+        self.returntomenu = button.Button(0, 200, 250, 40, "MC", return_action, "Return to main menu")
 
         self.key_left = button.Button(-50, -50, 130, 40, "MC", label="Left Key :")
         self.key_right = button.Button(-50, 0, 130, 40, "MC", label="Right Key :")
         self.key_action = button.Button(-50, 50, 130, 40, "MC", label="Action Key :")
-        self.choice_key_left = button.Button(90, -50, 100, 40, "MC", self.newleftkey, chr(key.jkey_left))
-        self.choice_key_right = button.Button(90, 0, 100, 40, "MC", self.newrightkey, chr(key.jkey_right))
-        self.choice_key_action = button.Button(90, 50, 100, 40, "MC", self.newactionkey, chr(
+        self.choice_key_left = button.Button(90, -50, 100, 40, "MC", newleftkey, chr(key.jkey_left))
+        self.choice_key_right = button.Button(90, 0, 100, 40, "MC", newrightkey, chr(key.jkey_right))
+        self.choice_key_action = button.Button(90, 50, 100, 40, "MC", newactionkey, chr(
             key.action_key) if key.action_key != 32 else "space")
 
         self.widget_manager.add_widget(self.returntomenu)
@@ -37,38 +73,3 @@ class OptionState(state.State):
     def draw(self):
         pyray.clear_background(pyray.BLACK)
         self.widget_manager.draw()
-
-    def return_action(self):
-        self.manager.set_state(menustate.MenuState())
-
-        # TODO : instead of chr(temp put something to traduce it into the name of the key)
-        # if you even let me do that, we should add another stop condition to the while loop (in the case we do it properly we should also create a class inputbutton)
-    def newleftkey(self):
-        pyray.poll_input_events()
-        temp = 0
-        while (not temp):
-            temp = pyray.get_key_pressed()
-            pyray.poll_input_events()
-        if temp != 0 and temp != key.jkey_right:
-            key.jkey_left = temp
-            self.choice_key_left.label = chr(temp)
-
-    def newrightkey(self):
-        pyray.poll_input_events()
-        temp = 0
-        while (not temp):
-            temp = pyray.get_key_pressed()
-            pyray.poll_input_events()
-        if temp != 0 and temp != key.jkey_left:
-            key.jkey_right = temp
-            self.choice_key_right.label = chr(temp)
-
-    def newactionkey(self):
-        pyray.poll_input_events()
-        temp = 0
-        while (not temp):
-            temp = pyray.get_key_pressed()
-            pyray.poll_input_events()
-        if temp != 0 and temp != key.action_key:
-            key.action_key = temp
-            self.choice_key_action.label = chr(temp) if temp != 32 else "Space"
