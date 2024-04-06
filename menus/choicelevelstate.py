e562a0e7457ecb60bcddc4668aea160486b92e0f
orwenn22 <46846090+orwenn22@users.noreply.github.com>
2024-04-06 09:42:04 +0200

    ChoiceLevelState : make it not crash if the directories don't exist

--- menus/choicelevelstate.py
+++ menus/choicelevelstate.py
@@ -12,16 +12,29 @@ from menus import menustate
 class ChoiceLevelState(state.State):
     def __init__(self):
         super().__init__()
+        self.widget_manager = widgetmanager.WidgetManager()
+        self.list_preview: list[pyray.Texture] = []
+
+        # For checkerboard background
+        self.bg = tiledbackground.TiledBackground(res.menu_bg_sprite)
 
         def back_to_main_menu():
             main_menu = menustate.MenuState()
             main_menu.bg.set_scrolling(self.bg.scrolling.x, self.bg.scrolling.y)
             self.manager.set_state(main_menu)
 
+        # Add back to main menu button
+        back_button = tiledbutton.TiledButton(0, 10, 110, 40, "TL",
+                                              res.tiled_button_right_sprite, 8, 2,                                                  "Back", back_to_main_menu)
+        back_button.set_scrollable(False).center_text().set_hovering_color(pyray.YELLOW).set_font_color(pyray.WHITE)
+        self.widget_manager.add_widget(back_button)
+
+        if "maps" not in os.listdir("./"):
+            self.widget_manager.add_widget(label.Label(0, 0, "MC", "No maps folder :(", 20, pyray.RED))
+            return
+
         # Load the map list
         self.list_level_file: list[str] = os.listdir("maps/")
-        self.list_preview: list[pyray.Texture] = []
-        self.widget_manager = widgetmanager.WidgetManager()
 
         # Title (static)
         title = label.Label(0, 10, "TC", "Choose the level", 30, pyray.BLACK).set_scrollable(False)
@@ -30,10 +43,11 @@ class ChoiceLevelState(state.State):
         # Level buttons (scrollable)
         for i in range(len(self.list_level_file)):
             level_name = self.list_level_file[i][:-4]       # TODO : function to cleanly remove the extension ?
-            if level_name + "_preview.png" in os.listdir("preview/"):
-                self.list_preview.append(pyray.load_texture("preview/" + level_name + "_preview.png"))
+
+            if "preview" not in os.listdir("./") or level_name + "_preview.png" not in os.listdir("preview/"):
+                self.list_preview.append(pyray.load_texture("res/default.png"))  # loading the default preview
             else:
-                self.list_preview.append(pyray.load_texture("res/default.png"))     # loading the default preview
+                self.list_preview.append(pyray.load_texture("preview/" + level_name + "_preview.png"))
 
             # Make and add the actual level entry in the menu
             level_button = previewbutton.PreviewButton(0, i*120, 300, 100, "MC",
@@ -45,16 +59,6 @@ class ChoiceLevelState(state.State):
         # Configure scrolling on the widget manager
         self.widget_manager.set_scrolling_proportions(0, 0, -len(self.list_level_file)*120, 0)
         self.widget_manager.set_scrolling_flags(False, True)
-
-        # Add back to main menu button
-        back_button = tiledbutton.TiledButton(0, 10, 110, 40, "TL",
-                                              res.tiled_button_right_sprite, 8, 2,
-                                              "Back", back_to_main_menu)
-        back_button.set_scrollable(False).center_text().set_hovering_color(pyray.YELLOW).set_font_color(pyray.WHITE)
-        self.widget_manager.add_widget(back_button)
-
-        # For checkerboard background
-        self.bg = tiledbackground.TiledBackground(res.menu_bg_sprite)
 
     def unload_ressources(self):
         for i in self.list_preview:
