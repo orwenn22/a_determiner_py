cab4779cbcb850ac32342fb737aadb5f7a9e9be3
orwenn22 <46846090+orwenn22@users.noreply.github.com>
2024-05-04 15:11:40 +0200

    Tooltip : fix bug + improve code readability

--- engine/tooltip/tooltiptext.py
+++ engine/tooltip/tooltiptext.py
@@ -3,13 +3,13 @@ from . import tooltipelements
 
     
 class TooltipText(tooltipelements.TooltipElement):
-    def __init__(self, text : str, fontsize : int, color: pyray.Color):
+    def __init__(self, text: str, fontsize: int, color: pyray.Color):
         self.text = text
         self.fontsize = fontsize
         self.color = color
 
         spacing = fontsize/10
-        text_size : pyray.Vector2 = pyray.measure_text_ex(pyray.get_font_default(), text, fontsize, spacing)
+        text_size: pyray.Vector2 = pyray.measure_text_ex(pyray.get_font_default(), text, fontsize, spacing)
         super().__init__(int(text_size.x), int(text_size.y))
 
     def draw(self, x: int, y: int):
